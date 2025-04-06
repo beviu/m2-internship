72a4b2256b10a109884a97f4e3a08ab1bcd91796
beviu <contact@beviu.com>
2025-04-06 09:55:42 +0200

    Work in progress new measurement code without printk

--- page-fault-timings/collect.c
+++ page-fault-timings/collect.c
@@ -1,10 +1,10 @@
 #define _GNU_SOURCE
+#include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
 #include <linux/userfaultfd.h>
 #include <pthread.h>
 #include <sched.h>
-#include <stdatomic.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,326 +14,40 @@
 #include <sys/mman.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <x86intrin.h>
 
-#define SYSLOG_ACTION_READ_ALL 3
-#define SYSLOG_ACTION_SIZE_BUFFER 10
-
-static uint64_t rdtsc() {
-  uint32_t eax, edx;
-
-#ifndef RDTSC_UNSERIALIZED
-  asm volatile("rdtsc" : "=a"(eax), "=d"(edx));
-#else
-  asm volatile("mfence; lfence\n"
-               "rdtsc\n"
-               "lfence"
-               : "=a"(eax), "=d"(edx));
+#ifdef USERFAULTFD
+#include <stdatomic.h>
 #endif
 
-  return eax | ((uint64_t)edx << 32);
+static bool read_timestamp(int dir_fd, const char *name, uint64_t *timestamp) {
+  int fd;
+  char buf[20];
+  ssize_t n;
+  
+  fd = openat(dir_fd, name, O_RDONLY);
+  if (fd == -1) {
+    fprintf(stderr, "open(/proc/sys/debug/fast-tracepoints/%s): %s\n", name, strerror(errno));
+    return false;
+  }
+
+  n = read(fd, buf, sizeof(buf));
+  if (n == -1) {
+    fprintf(stderr, "read(/proc/sys/debug/fast-tracepoints/%s): %s\n", name, strerror(errno));
+    close(fd);
+    return false;
+  }
+
+  *timestamp = 0;
+
+  close(fd);
+  
+  return true;
 }
 
+#ifdef USERFAULTFD
 static int userfaultfd(int flags) { return syscall(SYS_userfaultfd, flags); }
 
-enum parse_result {
-  OK,
-  ERROR,
-  EARLY_END,
-};
-
-static enum parse_result
-expect_character(const char **buffer, const char *buffer_end, char expected) {
-  if (*buffer == buffer_end)
-    return EARLY_END;
-
-  return *(*buffer)++ == expected ? OK : ERROR;
-}
-
-static enum parse_result expect_string(const char **buffer,
-                                       const char *buffer_end,
-                                       const char *expected) {
-  while (*expected != '\0') {
-    if (*buffer == buffer_end)
-      return EARLY_END;
-
-    if (*(*buffer)++ != *expected++)
-      return ERROR;
-  }
-
-  return OK;
-}
-
-static enum parse_result expect_delimited_field(const char **buffer,
-                                                const char *buffer_end,
-                                                char start, char end) {
-  enum parse_result result = expect_character(buffer, buffer_end, start);
-  if (result != OK)
-    return result;
-
-  for (;;) {
-    if (*buffer == buffer_end)
-      return EARLY_END;
-
-    if (*(*buffer)++ == end)
-      break;
-  }
-
-  return OK;
-}
-
-static enum parse_result read_u64(const char **buffer, const char *buffer_end,
-                                  uint64_t *value) {
-  if (*buffer == buffer_end)
-    return EARLY_END;
-
-  if (**buffer < '0' || **buffer > '9')
-    return ERROR;
-
-  *value = *(*buffer)++ - '0';
-
-  while (*buffer != buffer_end && **buffer >= '0' && **buffer <= '9') {
-    *value *= 10;
-    *value += *(*buffer)++ - '0';
-  }
-
-  return OK;
-}
-
-static enum parse_result read_timestamp(const char **buffer,
-                                        const char *buffer_end,
-                                        struct timeval *timestamp) {
-  uint64_t value;
-
-  enum parse_result result = read_u64(buffer, buffer_end, &value);
-  if (result != OK)
-    return result;
-
-  timestamp->tv_sec = value;
-
-  result = expect_character(buffer, buffer_end, '.');
-  if (result != OK)
-    return result;
-
-  result = read_u64(buffer, buffer_end, &value);
-  if (result != OK)
-    return result;
-
-  timestamp->tv_usec = result;
-
-  return OK;
-}
-
-static enum parse_result read_timestamp_part(const char **buffer,
-                                             const char *buffer_end,
-                                             struct timeval *timestamp) {
-  enum parse_result result = expect_character(buffer, buffer_end, '[');
-  if (result != OK)
-    return result;
-
-  /* Skip spaces. */
-  for (;;) {
-    if (*buffer == buffer_end)
-      return EARLY_END;
-
-    if (**buffer != ' ')
-      break;
-
-    ++(*buffer);
-  }
-
-  result = read_timestamp(buffer, buffer_end, timestamp);
-  if (result != OK)
-    return result;
-
-  result = expect_character(buffer, buffer_end, ']');
-  if (result != OK)
-    return result;
-
-  return expect_character(buffer, buffer_end, ' ');
-}
-
-struct log_line {
-  const char *body;
-  const char *body_end;
-  struct timeval timestamp;
-};
-
-static enum parse_result read_log_line(const char **buffer,
-                                       const char *buffer_end,
-                                       struct log_line *line) {
-  /* Optional field. */
-  const char *tmp = *buffer;
-  enum parse_result result = expect_delimited_field(&tmp, buffer_end, '<', '>');
-  if (result == OK)
-    *buffer = tmp;
-
-  tmp = *buffer;
-  result = read_timestamp_part(&tmp, buffer_end, &line->timestamp);
-  if (result == OK) {
-    *buffer = tmp;
-  } else {
-    line->timestamp.tv_sec = 0;
-    line->timestamp.tv_usec = 0;
-  }
-
-  tmp = *buffer;
-  result = expect_delimited_field(&tmp, buffer_end, '[', ']');
-  if (result == OK) {
-    result = expect_character(&tmp, buffer_end, ' ');
-    if (result == OK)
-      *buffer = tmp;
-  }
-
-  line->body = *buffer;
-
-  for (;;) {
-    if (*buffer == buffer_end)
-      return EARLY_END;
-
-    if (**buffer == '\n')
-      break;
-
-    ++(*buffer);
-  }
-
-  line->body_end = (*buffer)++;
-
-  return OK;
-}
-
-struct section {
-  uint64_t start_tsc;
-  uint64_t end_tsc;
-};
-
-struct named_section {
-  const char *name;
-  const char *name_end;
-  struct section section;
-};
-
-static enum parse_result read_named_section(const char **buffer,
-                                            const char *buffer_end,
-                                            struct named_section *section) {
-  section->name = *buffer;
-
-  for (;;) {
-    if (*buffer == buffer_end)
-      return EARLY_END;
-
-    if (**buffer == ':')
-      break;
-
-    ++(*buffer);
-  }
-
-  section->name_end = (*buffer)++;
-
-  enum parse_result result = expect_string(buffer, buffer_end, " start=");
-  if (result != OK)
-    return result;
-
-  result = read_u64(buffer, buffer_end, &section->section.start_tsc);
-  if (result != OK)
-    return result;
-
-  result = expect_string(buffer, buffer_end, ", end=");
-  if (result != OK)
-    return result;
-
-  return read_u64(buffer, buffer_end, &section->section.end_tsc);
-}
-
-struct sections {
-  struct section save_state;
-  struct section read_lock_vma;
-  struct section walk_page_table;
-  struct section handle_mm_fault;
-  struct section cleanup;
-#ifdef USERFAULTFD
-  struct section wake_up_userfaultfd;
-  struct section retry_read_lock_vma;
-  struct section retry_walk_page_table;
-  struct section retry_handle_mm_fault;
-#endif
-};
-
-static bool compare_section_name(const struct named_section *section,
-                                 const char *with) {
-  const char *name = section->name;
-
-  while (*with != '\0') {
-    if (name == section->name_end || *name++ != *with++)
-      return false;
-  }
-
-  return name == section->name_end;
-}
-
-static void process_klog_buffer(const char *buffer, int length,
-                                struct sections *sections,
-                                struct timeval *seen) {
-  const char *buffer_end = buffer + length;
-
-  for (;;) {
-    struct log_line line;
-
-    const char *tmp = buffer;
-    enum parse_result result = read_log_line(&tmp, buffer_end, &line);
-    switch (result) {
-    case OK:
-      buffer = tmp;
-      break;
-    case ERROR:
-      fprintf(stderr, "Failed to parse kernel log line: %s\n", buffer);
-      return;
-    case EARLY_END:
-      return;
-    }
-
-    if (line.timestamp.tv_sec == 0 && line.timestamp.tv_usec == 0)
-      continue;
-
-    if (seen->tv_sec != 0 && seen->tv_usec != 0) {
-      if ((line.timestamp.tv_sec < seen->tv_sec) ||
-          (line.timestamp.tv_sec == seen->tv_sec &&
-           line.timestamp.tv_usec <= seen->tv_usec))
-        continue;
-    }
-
-    *seen = line.timestamp;
-
-    struct named_section section;
-    result = read_named_section(&line.body, line.body_end, &section);
-    if (result == OK) {
-      if (compare_section_name(&section, "save_state")) {
-        sections->save_state = section.section;
-      } else if (compare_section_name(&section, "read_lock_vma")) {
-        sections->read_lock_vma = section.section;
-      } else if (compare_section_name(&section, "walk_page_table")) {
-        sections->walk_page_table = section.section;
-      } else if (compare_section_name(&section, "handle_mm_fault")) {
-        sections->handle_mm_fault = section.section;
-      } else if (compare_section_name(&section, "cleanup")) {
-        sections->cleanup = section.section;
-      } else {
-#ifdef USERFAULTFD
-        if (compare_section_name(&section, "wake_up_userfaultfd")) {
-          sections->wake_up_userfaultfd = section.section;
-        } else if (compare_section_name(&section, "retry_read_lock_vma")) {
-          sections->retry_read_lock_vma = section.section;
-        } else if (compare_section_name(&section, "retry_walk_page_table")) {
-          sections->retry_walk_page_table = section.section;
-        } else if (compare_section_name(&section, "retry_handle_mm_fault")) {
-          sections->retry_handle_mm_fault = section.section;
-        }
-#endif
-      }
-    }
-  }
-}
-
-#ifdef USERFAULTFD
 struct thread_args {
   int fd;
   struct uffdio_range range;
@@ -373,16 +87,14 @@ static void *userfaultfd_thread_routine(void *data) {
 
 int main() {
   int ret = EXIT_SUCCESS;
+  int tracepoints_dir_fd = -1;
   cpu_set_t cpu_set;
   ssize_t page_size;
-  int klog_size;
-  char *klog_buffer = NULL;
   void *page = MAP_FAILED;
   int fd = -1;
   struct uffdio_api api_args = {0};
   struct uffdio_register register_args = {0};
   int err;
-  struct sections sections;
   uint64_t page_fault_tsc;
   uint32_t restore_state_start_tsc_1;
   uint32_t restore_state_start_tsc_2;
@@ -401,6 +113,13 @@ int main() {
   uint64_t msg_received_tsc_copy;
 #endif
 
+  tracepoints_dir_fd = open("/proc/sys/debug/fast-tracepoints", O_DIRECTORY | O_PATH);
+  if (tracepoints_dir_fd == -1) {
+    perror("open(/proc/sys/debug/fast-tracepoints)");
+    ret = EXIT_FAILURE;
+    goto out;
+  }
+
   CPU_ZERO(&cpu_set);
   CPU_SET(0, &cpu_set);
   err = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set), &cpu_set);
@@ -413,20 +132,6 @@ int main() {
   page_size = sysconf(_SC_PAGESIZE);
   if (page_size == -1) {
     perror("sysconf(_SC_PAGESIZE)");
-    ret = EXIT_FAILURE;
-    goto out;
-  }
-
-  klog_size = klogctl(SYSLOG_ACTION_SIZE_BUFFER, NULL, 0);
-  if (klog_size == -1) {
-    perror("klogctl(SYSLOG_ACTION_SIZE_BUFFER)");
-    ret = EXIT_FAILURE;
-    goto out;
-  }
-
-  klog_buffer = malloc(klog_size);
-  if (klog_buffer == NULL) {
-    fputs("Failed to allocate memory for the kernel log buffer.\n", stderr);
     ret = EXIT_FAILURE;
     goto out;
   }
@@ -622,12 +327,12 @@ out:
     pthread_join(userfaultfd_thread, NULL);
   }
 #endif
-  if (fd == -1)
+  if (fd != -1)
     close(fd);
   if (page)
     munmap(page, page_size);
-  if (klog_buffer)
-    free(klog_buffer);
+  if (tracepoints_dir_fd != -1)
+    close(tracepoints_dir_fd);
 
   return ret;
 }
