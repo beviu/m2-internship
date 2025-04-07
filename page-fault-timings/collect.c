0ffa6e3f5a34b02f04e645ea192bb835854b3405
beviu <contact@beviu.com>
2025-04-07 13:56:24 +0200

    Finish measurement code without printk

--- page-fault-timings/collect.c
+++ page-fault-timings/collect.c
@@ -20,49 +20,77 @@
 #include <stdatomic.h>
 #endif
 
-static bool read_timestamp(int dir_fd, const char *name, uint64_t *timestamp) {
-  int fd;
-  char buf[20];
-  ssize_t n;
-  
-  fd = openat(dir_fd, name, O_RDONLY);
-  if (fd == -1) {
-    fprintf(stderr, "open(/proc/sys/debug/fast-tracepoints/%s): %s\n", name, strerror(errno));
+static bool parse_uint64_t(const char **buf, const char *end, uint64_t *out) {
+  if (*buf == end || **buf < '0' || **buf > '9')
     return false;
+
+  *out = 0;
+
+  while (*buf != end && **buf >= '0' && **buf <= '9') {
+    *out *= 10;
+    *out += **buf - '0';
+    ++*buf;
   }
 
-  n = read(fd, buf, sizeof(buf));
-  if (n == -1) {
-    fprintf(stderr, "read(/proc/sys/debug/fast-tracepoints/%s): %s\n", name, strerror(errno));
-    close(fd);
-    return false;
-  }
-
-  *timestamp = 0;
-
-  close(fd);
-  
   return true;
+}
+
+static int timestamps_dir_fd = -1;
+
+static bool read_timestamp(const char *name, uint64_t *timestamp) {
+  int timestamp_fd;
+  char buf[21];
+  ssize_t n;
+  const char *tmp;
+  bool ok = false;
+
+  timestamp_fd = openat(timestamps_dir_fd, name, O_RDONLY);
+  if (timestamp_fd == -1) {
+    fprintf(stderr, "open(/proc/sys/debug/fast-tracepoints/%s): %s\n", name,
+            strerror(errno));
+    goto fail;
+  }
+
+  n = read(timestamp_fd, buf, sizeof(buf));
+  if (n == -1) {
+    fprintf(stderr, "read(/proc/sys/debug/fast-tracepoints/%s): %s\n", name,
+            strerror(errno));
+    goto fail;
+  }
+
+  tmp = buf;
+  if (!parse_uint64_t(&tmp, buf + n, timestamp)) {
+    fprintf(stderr, "Failed to parse timestamp: %.*s.\n", (int)n, buf);
+    goto fail;
+  }
+
+  ok = true;
+
+fail:
+  close(timestamp_fd);
+
+  return ok;
 }
 
 #ifdef USERFAULTFD
 static int userfaultfd(int flags) { return syscall(SYS_userfaultfd, flags); }
 
-struct thread_args {
-  int fd;
+struct userfaultfd_thread_args {
+  int userfaultfd_fd;
   struct uffdio_range range;
 };
 
-static _Atomic uint64_t msg_received_tsc;
+static _Atomic uint64_t timestamp_msg_received;
 
 static void *userfaultfd_thread_routine(void *data) {
   size_t n;
-  struct thread_args *args = (struct thread_args *)data;
+  struct userfaultfd_thread_args *args = (struct userfaultfd_thread_args *)data;
   struct uffd_msg msg;
-  struct uffdio_zeropage zeropage;
+  unsigned int aux;
+  struct uffdio_zeropage zeropage_args = {0};
 
   for (;;) {
-    n = read(args->fd, &msg, sizeof(msg));
+    n = read(args->userfaultfd_fd, &msg, sizeof(msg));
     if (n == -1) {
       perror("read");
       break;
@@ -71,11 +99,12 @@ static void *userfaultfd_thread_routine(void *data) {
       break;
     }
 
-    atomic_store_explicit(&msg_received_tsc, rdtsc(), memory_order_relaxed);
+    atomic_store_explicit(&timestamp_msg_received, __rdtscp(&aux),
+                          memory_order_relaxed);
 
-    zeropage.range = args->range;
-    zeropage.mode = 0;
-    if (ioctl(args->fd, UFFDIO_ZEROPAGE, &zeropage) == -1) {
+    zeropage_args.range = args->range;
+    zeropage_args.mode = 0;
+    if (ioctl(args->userfaultfd_fd, UFFDIO_ZEROPAGE, &zeropage_args) == -1) {
       perror("ioctl(UFFDIO_ZEROPAGE)");
       break;
     }
@@ -86,38 +115,49 @@ static void *userfaultfd_thread_routine(void *data) {
 #endif
 
 int main() {
-  int ret = EXIT_SUCCESS;
-  int tracepoints_dir_fd = -1;
+  int ret = EXIT_FAILURE;
   cpu_set_t cpu_set;
   ssize_t page_size;
   void *page = MAP_FAILED;
-  int fd = -1;
+  int err;
+  unsigned int aux;
+  int i;
+  uint64_t timestamp_page_fault;
+  uint8_t zero;
+  uint32_t timestamp_iret_1;
+  uint32_t timestamp_iret_2;
+  uint32_t timestamp_isr_entry_1;
+  uint32_t timestamp_isr_entry_2;
+  uint64_t timestamp_end;
+  uint64_t timestamp_iret;
+  uint64_t timestamp_isr_entry;
+  uint64_t timestamp_lock_vma_under_rcu_start;
+  uint64_t timestamp_lock_vma_under_rcu_end;
+  uint64_t timestamp_first_handle_mm_fault_start;
+  uint64_t timestamp_first_page_table_walk_end;
+  uint64_t timestamp_first_handle_mm_fault_end;
+#ifdef USERFAULTFD
+  int userfaultfd_fd = -1;
   struct uffdio_api api_args = {0};
   struct uffdio_register register_args = {0};
-  int err;
-  uint64_t page_fault_tsc;
-  uint32_t restore_state_start_tsc_1;
-  uint32_t restore_state_start_tsc_2;
-  uint32_t iret_tsc_1;
-  uint32_t iret_tsc_2;
-  uint64_t end_tsc;
-  uint64_t restore_state_start_tsc;
-  uint64_t iret_tsc;
-  int read;
-  struct timeval seen;
-#ifdef USERFAULTFD
-  struct thread_args thread_args;
+  struct userfaultfd_thread_args thread_args;
   pthread_t userfaultfd_thread;
   bool started_userfaultfd_thread = false;
-  struct uffd_msg msg;
-  uint64_t msg_received_tsc_copy;
+  uint64_t timestamp_msg_received_copy;
+  uint64_t timestamp_wake_up_userfaultfd_start;
+  uint64_t timestamp_wake_up_userfaultfd_end;
+  uint64_t timestamp_lock_mm_and_find_vma_start;
+  uint64_t timestamp_lock_mm_and_find_vma_end;
+  uint64_t timestamp_second_handle_mm_fault_start;
+  uint64_t timestamp_second_page_table_walk_end;
+  uint64_t timestamp_second_handle_mm_fault_end;
 #endif
 
-  tracepoints_dir_fd = open("/proc/sys/debug/fast-tracepoints", O_DIRECTORY | O_PATH);
-  if (tracepoints_dir_fd == -1) {
+  timestamps_dir_fd =
+      open("/proc/sys/debug/fast-tracepoints", O_DIRECTORY | O_PATH);
+  if (timestamps_dir_fd == -1) {
     perror("open(/proc/sys/debug/fast-tracepoints)");
-    ret = EXIT_FAILURE;
-    goto out;
+    goto fail;
   }
 
   CPU_ZERO(&cpu_set);
@@ -125,56 +165,49 @@ int main() {
   err = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set), &cpu_set);
   if (err) {
     fprintf(stderr, "pthread_setaffinity_np: %s\n", strerror(err));
-    ret = EXIT_FAILURE;
-    goto out;
+    goto fail;
   }
 
   page_size = sysconf(_SC_PAGESIZE);
   if (page_size == -1) {
     perror("sysconf(_SC_PAGESIZE)");
-    ret = EXIT_FAILURE;
-    goto out;
+    goto fail;
   }
 
   page = mmap(NULL, page_size, PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
   if (page == MAP_FAILED) {
-    perror("mmap");
-    ret = EXIT_FAILURE;
-    goto out;
+    fputs("Failed to allocate a page!\n", stderr);
+    goto fail;
   }
 
 #ifdef USERFAULTFD
-  fd = userfaultfd(O_CLOEXEC);
-  if (fd == -1) {
+  userfaultfd_fd = userfaultfd(O_CLOEXEC);
+  if (userfaultfd_fd == -1) {
     perror("userfaultfd");
-    ret = EXIT_FAILURE;
-    goto out;
+    goto fail;
   }
 
   api_args.api = UFFD_API;
-  if (ioctl(fd, UFFDIO_API, &api_args) == -1) {
+  if (ioctl(userfaultfd_fd, UFFDIO_API, &api_args) == -1) {
     perror("ioctl(UFFDIO_API)");
-    ret = EXIT_FAILURE;
-    goto out;
+    goto fail;
   }
 
   register_args.range.start = (uint64_t)page;
   register_args.range.len = page_size;
   register_args.mode = UFFDIO_REGISTER_MODE_MISSING;
-  if (ioctl(fd, UFFDIO_REGISTER, &register_args) == -1) {
+  if (ioctl(userfaultfd_fd, UFFDIO_REGISTER, &register_args) == -1) {
     perror("ioctl(UFFDIO_REGISTER)");
-    ret = EXIT_FAILURE;
-    goto out;
+    goto fail;
   }
 
-  thread_args.fd = fd;
+  thread_args.userfaultfd_fd = userfaultfd_fd;
   thread_args.range = register_args.range;
   err = pthread_create(&userfaultfd_thread, NULL, userfaultfd_thread_routine,
                        &thread_args);
   if (err) {
     fprintf(stderr, "pthread_create: %s\n", strerror(err));
-    ret = EXIT_FAILURE;
-    goto out;
+    goto fail;
   }
 
   started_userfaultfd_thread = true;
@@ -186,153 +219,117 @@ int main() {
   err = pthread_setaffinity_np(userfaultfd_thread, sizeof(cpu_set), &cpu_set);
   if (err) {
     fprintf(stderr, "pthread_setaffinity_np: %s\n", strerror(err));
-    ret = EXIT_FAILURE;
-    goto out;
+    goto fail;
   }
 
-  puts("save_state_start, save_state_end, read_lock_vma_start, "
-       "read_lock_vma_end, walk_page_table_start, walk_page_table_end, "
+  puts("isr_entry, lock_vma_under_rcu_start, lock_vma_under_rcu_end, "
+       "first_handle_mm_fault_start, first_page_table_walk_end, "
        "wake_up_userfaultfd_start, wake_up_userfaultfd_end, "
-       "handle_mm_fault_start, msg_received, handle_mm_fault_end, "
-       "retry_read_lock_vma_start, retry_read_lock_vma_end, "
-       "retry_walk_page_table_start, retry_walk_page_table_end, "
-       "retry_handle_mm_fault_start, retry_handle_mm_fault_end, "
-       "cleanup_start, cleanup_end, restore_state_start, iret, end");
+       "first_handle_mm_fault_end, lock_mm_and_find_vma_start, "
+       "lock_mm_and_find_vma_end, second_handle_mm_fault_start, "
+       "second_page_table_walk_end, second_handle_mm_fault_end, iret, end");
 #else
-  puts("save_state_start, save_state_end, read_lock_vma_start, "
-       "read_lock_vma_end, walk_page_table_start, walk_page_table_end, "
-       "handle_mm_fault_start, handle_mm_fault_end, "
-       "cleanup_start, cleanup_end, restore_state_start, iret, end");
+  puts("isr_entry, lock_vma_under_rcu_start, lock_vma_under_rcu_end, "
+       "first_handle_mm_fault_start, first_page_table_walk_end, "
+       "first_handle_mm_fault_end, iret, end");
 #endif
 
-  for (int i = 0; i < 100000; ++i) {
-    page_fault_tsc = rdtsc();
+  for (i = 0; i < 100000; ++i) {
+    timestamp_page_fault = __rdtscp(&aux);
 
     /* Trigger a page fault with the magic value in ebx. */
-    asm volatile("movb (%[page]), %%dil"
-                 : "=a"(iret_tsc_1), "=d"(iret_tsc_2),
-                   "=b"(restore_state_start_tsc_1),
-                   "=c"(restore_state_start_tsc_2)
+    asm volatile("movb (%[page]), %[zero]"
+                 : "=&a"(timestamp_iret_1), "=&d"(timestamp_iret_2),
+                   "=&D"(timestamp_isr_entry_1),
+                   "=&S"(timestamp_isr_entry_2), [zero] "=r"(zero)
                  : [page] "r"(page), "b"(0xb141a52a)
-                 : "rdi");
+                 : "ecx");
 
-    end_tsc = rdtsc();
+    timestamp_end = __rdtscp(&aux);
 
-    read = klogctl(SYSLOG_ACTION_READ_ALL, klog_buffer, klog_size);
-    if (read == -1) {
-      perror("klogctl(SYSLOG_ACTION_READ_ALL)");
-      ret = EXIT_FAILURE;
-      goto out;
-    }
+    timestamp_iret = timestamp_iret_1 | ((uint64_t)timestamp_iret_2 << 32);
+    timestamp_isr_entry =
+        timestamp_isr_entry_1 | ((uint64_t)timestamp_isr_entry_2 << 32);
 
-    sections = (struct sections){0};
-    process_klog_buffer(klog_buffer, read, &sections, &seen);
+#define R(name) read_timestamp(#name, &timestamp_##name)
 
-    if ((sections.save_state.start_tsc == 0 &&
-         sections.save_state.end_tsc == 0) ||
-        (sections.read_lock_vma.start_tsc == 0 &&
-         sections.read_lock_vma.end_tsc == 0) ||
-        (sections.walk_page_table.start_tsc == 0 &&
-         sections.walk_page_table.end_tsc == 0) ||
-        (sections.handle_mm_fault.start_tsc == 0 &&
-         sections.handle_mm_fault.end_tsc == 0) ||
-        (sections.cleanup.start_tsc == 0 && sections.cleanup.end_tsc == 0)
-#ifdef USERFAULTFD
-        || (sections.wake_up_userfaultfd.start_tsc == 0 &&
-            sections.wake_up_userfaultfd.end_tsc == 0) ||
-        (sections.retry_read_lock_vma.start_tsc == 0 &&
-         sections.retry_read_lock_vma.end_tsc == 0) ||
-        (sections.retry_walk_page_table.start_tsc == 0 &&
-         sections.retry_walk_page_table.end_tsc == 0) ||
-        (sections.retry_handle_mm_fault.start_tsc == 0 &&
-         sections.retry_handle_mm_fault.end_tsc == 0)
-#endif
-    ) {
-      fputs("Missing timings in kernel log buffer.\n", stderr);
-      munmap(page, page_size);
-      free(klog_buffer);
-      return EXIT_FAILURE;
-    }
-
-    restore_state_start_tsc =
-        restore_state_start_tsc_1 | ((uint64_t)restore_state_start_tsc_2 << 32);
-    iret_tsc = iret_tsc_1 | ((uint64_t)iret_tsc_2 << 32);
+    if (!R(lock_vma_under_rcu_start) || !R(lock_vma_under_rcu_end) ||
+        !R(first_handle_mm_fault_start) || !R(first_page_table_walk_end) ||
+        !R(first_handle_mm_fault_end))
+      goto fail;
 
 #ifdef USERFAULTFD
-    msg_received_tsc_copy =
-        atomic_load_explicit(&msg_received_tsc, memory_order_relaxed);
-    if (msg_received_tsc_copy == 0) {
+    timestamp_msg_received_copy =
+        atomic_load_explicit(&timestamp_msg_received, memory_order_relaxed);
+    if (timestamp_msg_received_copy == 0) {
       fputs("userfaultfd thread was not notified.\n", stderr);
       ret = EXIT_FAILURE;
-      goto out;
+      goto fail;
     }
 
+    if (!R(wake_up_userfaultfd_start) || !R(wake_up_userfaultfd_end) ||
+        !R(lock_mm_and_find_vma_start) || !R(lock_mm_and_find_vma_end) ||
+        !R(second_handle_mm_fault_start) || !R(second_page_table_walk_end) ||
+        !R(second_handle_mm_fault_end))
+      goto fail;
+
     printf("%" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64
            ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64
            ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64
-           ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64
-           ", %" PRIu64 ", %" PRIu64 "\n",
-           sections.save_state.start_tsc - page_fault_tsc,
-           sections.save_state.end_tsc - page_fault_tsc,
-           sections.read_lock_vma.start_tsc - page_fault_tsc,
-           sections.read_lock_vma.end_tsc - page_fault_tsc,
-           sections.walk_page_table.start_tsc - page_fault_tsc,
-           sections.walk_page_table.end_tsc - page_fault_tsc,
-           sections.wake_up_userfaultfd.start_tsc - page_fault_tsc,
-           sections.wake_up_userfaultfd.end_tsc - page_fault_tsc,
-           sections.handle_mm_fault.start_tsc - page_fault_tsc,
-           msg_received_tsc_copy - page_fault_tsc,
-           sections.handle_mm_fault.end_tsc - page_fault_tsc,
-           sections.retry_read_lock_vma.start_tsc - page_fault_tsc,
-           sections.retry_read_lock_vma.end_tsc - page_fault_tsc,
-           sections.retry_walk_page_table.start_tsc - page_fault_tsc,
-           sections.retry_walk_page_table.end_tsc - page_fault_tsc,
-           sections.retry_handle_mm_fault.start_tsc - page_fault_tsc,
-           sections.retry_handle_mm_fault.end_tsc - page_fault_tsc,
-           sections.cleanup.start_tsc - page_fault_tsc,
-           sections.cleanup.end_tsc - page_fault_tsc,
-           restore_state_start_tsc - page_fault_tsc, iret_tsc - page_fault_tsc,
-           end_tsc - page_fault_tsc);
+           "\n",
+           timestamp_isr_entry - timestamp_page_fault,
+           timestamp_lock_vma_under_rcu_start - timestamp_page_fault,
+           timestamp_lock_vma_under_rcu_end - timestamp_page_fault,
+           timestamp_first_handle_mm_fault_start - timestamp_page_fault,
+           timestamp_first_page_table_walk_end - timestamp_page_fault,
+           timestamp_wake_up_userfaultfd_start - timestamp_page_fault,
+           timestamp_wake_up_userfaultfd_end - timestamp_page_fault,
+           timestamp_first_handle_mm_fault_end - timestamp_page_fault,
+           timestamp_lock_mm_and_find_vma_start - timestamp_page_fault,
+           timestamp_lock_mm_and_find_vma_end - timestamp_page_fault,
+           timestamp_second_handle_mm_fault_start - timestamp_page_fault,
+           timestamp_second_page_table_walk_end - timestamp_page_fault,
+           timestamp_second_handle_mm_fault_end - timestamp_page_fault,
+           timestamp_iret - timestamp_page_fault,
+           timestamp_end - timestamp_page_fault);
 
-    atomic_store_explicit(&msg_received_tsc, 0, memory_order_relaxed);
+    atomic_store_explicit(&timestamp_msg_received, 0, memory_order_relaxed);
 #else
     printf("%" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64
-           ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64
            ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 "\n",
-           sections.save_state.start_tsc - page_fault_tsc,
-           sections.save_state.end_tsc - page_fault_tsc,
-           sections.read_lock_vma.start_tsc - page_fault_tsc,
-           sections.read_lock_vma.end_tsc - page_fault_tsc,
-           sections.walk_page_table.start_tsc - page_fault_tsc,
-           sections.walk_page_table.end_tsc - page_fault_tsc,
-           sections.handle_mm_fault.start_tsc - page_fault_tsc,
-           sections.handle_mm_fault.end_tsc - page_fault_tsc,
-           sections.cleanup.start_tsc - page_fault_tsc,
-           sections.cleanup.end_tsc - page_fault_tsc,
-           restore_state_start_tsc - page_fault_tsc, iret_tsc - page_fault_tsc,
-           end_tsc - page_fault_tsc);
+           timestamp_isr_entry - timestamp_page_fault,
+           timestamp_lock_vma_under_rcu_start - timestamp_page_fault,
+           timestamp_lock_vma_under_rcu_end - timestamp_page_fault,
+           timestamp_first_handle_mm_fault_start - timestamp_page_fault,
+           timestamp_first_page_table_walk_end - timestamp_page_fault,
+           timestamp_first_handle_mm_fault_end - timestamp_page_fault,
+           timestamp_iret - timestamp_page_fault,
+           timestamp_end - timestamp_page_fault);
 #endif
+
+#undef R
 
     if (madvise(page, page_size, MADV_DONTNEED) == -1) {
       perror("mmap");
-      ret = EXIT_FAILURE;
-      goto out;
+      goto fail;
     }
   }
 
-out:
+  ret = EXIT_SUCCESS;
+
+fail:
 #ifdef USERFAULTFD
   if (started_userfaultfd_thread) {
     pthread_cancel(userfaultfd_thread);
     pthread_join(userfaultfd_thread, NULL);
   }
+  if (userfaultfd_fd != -1)
+    close(userfaultfd_fd);
 #endif
-  if (fd != -1)
-    close(fd);
-  if (page)
+  if (page != MAP_FAILED)
     munmap(page, page_size);
-  if (tracepoints_dir_fd != -1)
-    close(tracepoints_dir_fd);
+  if (timestamps_dir_fd != -1)
+    close(timestamps_dir_fd);
 
   return ret;
 }
