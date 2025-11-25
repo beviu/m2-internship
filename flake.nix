b103606a61e094169624ff0228acc57977597e2c
beviu <contact@beviu.com>
2025-11-25 16:15:18 +0100

    Add Nix derivation to download Twitter dataset used by ExtMem

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
         {
           extmem-ufault = pkgs.callPackage ./pkgs/extmem-ufault/package.nix { };
           gapbs = pkgs.callPackage ./pkgs/gapbs/package.nix { };
+          twitter-dataset = pkgs.callPackage ./pkgs/twitter-dataset/package.nix { };
         }
       );
 
---
+++ pkgs/twitter-dataset/package.nix
@@ -0,0 +1,5 @@
+{ fetchzip }:
+
+fetchzip {
+  url = "https://vault.cs.uwaterloo.ca/s/7SEPzsQqybDyCTR/download?path=%2F&files=twitter_compressed.tar.gz";
+}
